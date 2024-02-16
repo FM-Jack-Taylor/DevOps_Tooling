885cca0057bbce9b59d1165df83a2f3d6c4207ac
FM-Jack-Taylor <124883225+FM-Jack-Taylor@users.noreply.github.com>
2024-02-16 11:30:14 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,2 +1,2 @@
-docker build -t flask-app -f ./var/lib/jenkins/workspace/Lab3/Task1/Dockerfile .
-docker build -t nginx -f ./var/lib/jenkins/workspace/Lab3/Task1/Dockerfile.nginx .
+docker build -t flask-app -f /var/lib/jenkins/workspace/Lab3/Task1/Dockerfile .
+docker build -t nginx -f /var/lib/jenkins/workspace/Lab3/Task1/Dockerfile.nginx .
