68d1afd5aab4b24b411068d74311053dac6e9e11
FM-Jack-Taylor <124883225+FM-Jack-Taylor@users.noreply.github.com>
2024-02-16 11:08:59 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,6 +1,6 @@
 ls
-docker build -t flask-app -f /Task1/Dockerfile .
-docker build -t nginx -f /Task1/Dockerfile.nginx .
+docker build -t flask-app -f ./Task1/Dockerfile
+docker build -t nginx -f ./Task1/Dockerfile.nginx
 
 docker run -d --name flask-app
 docker run -d -p 80:80 --name nginx nginx
