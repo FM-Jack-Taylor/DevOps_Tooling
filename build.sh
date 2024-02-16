030303fb7081a71af5ae1c4a41c651ad2ddcb1b1
FM-Jack-Taylor <124883225+FM-Jack-Taylor@users.noreply.github.com>
2024-02-16 10:03:56 +0000

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,6 @@
-echo 'building'
-mkdir ~/jenkins-test
-ls
+docker build -t myapp -f Dockerfile .
+docker build -t mynginx -f Dockerfile.nginx .
+
+docker run -d --name flask-app
+docker run -d -p 80:80 --name nginx mynginx
+
